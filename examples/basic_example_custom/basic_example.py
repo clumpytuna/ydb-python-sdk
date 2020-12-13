b1b39a75926c6979f194d88f861ed3addbfbfe54
Clumpy Tuna <anuhinm@mail.ru>
2020-12-13 21:40:57 +0300

    Add complex transactions

--- examples/basic_example_custom/basic_example.py
+++ examples/basic_example_custom/basic_example.py
@@ -56,6 +56,47 @@ FROM AS_TABLE($episodesData);
 """
 
 
+def describe_table(session, path, name):
+    result = session.describe_table(os.path.join(path, name))
+    print("\n> describe table: registers")
+    for column in result.columns:
+        print("column, name:", column.name, ",", str(column.type.item).strip())
+
+
+def bulk_upsert(table_client, path):
+    print("\n> bulk upsert: episodes")
+    column_types = ydb.BulkUpsertColumns() \
+        .add_column('series_id', ydb.OptionalType(ydb.PrimitiveType.Uint64)) \
+        .add_column('season_id', ydb.OptionalType(ydb.PrimitiveType.Uint64)) \
+        .add_column('episode_id', ydb.OptionalType(ydb.PrimitiveType.Uint64)) \
+        .add_column('title', ydb.OptionalType(ydb.PrimitiveType.Utf8)) \
+        .add_column('air_date', ydb.OptionalType(ydb.PrimitiveType.Uint64))
+    rows = basic_example_data.get_episodes_data_for_bulk_upsert()
+    table_client.bulk_upsert(os.path.join(path, "episodes"), rows, column_types)
+
+
+def is_directory_exists(driver, path):
+    try:
+        return driver.scheme_client.describe_path(path).is_directory()
+    except ydb.SchemeError:
+        return False
+
+
+def ensure_path_exists(driver, database, path):
+    paths_to_create = list()
+    path = path.rstrip("/")
+    while path not in ("", database):
+        full_path = os.path.join(database, path)
+        if is_directory_exists(driver, full_path):
+            break
+        paths_to_create.append(full_path)
+        path = os.path.dirname(path).rstrip("/")
+
+    while len(paths_to_create) > 0:
+        full_path = paths_to_create.pop(-1)
+        driver.scheme_client.make_directory(full_path)
+
+
 def fill_tables_with_data(session, path):
     global FillDataQuery
 
@@ -68,45 +109,6 @@ def fill_tables_with_data(session, path):
         },
         commit_tx=True,
     )
-
-
-def read(session, path, register_id):
-    # new transaction in serializable read write mode
-    # if query successfully completed you will get result sets.
-    # otherwise exception will be raised
-    result_sets = session.transaction(ydb.SerializableReadWrite()).execute(
-        """
-        PRAGMA TablePathPrefix("{}");
-        SELECT
-            register_id,
-            value,
-        FROM registers
-        WHERE register_id = {};
-        """.format(path, register_id),
-        commit_tx=True,
-    )
-    print("\n> read transaction:")
-    for row in result_sets[0].rows:
-        print("register, id: ", row.register_id, ", value: ", row.value)
-
-    return result_sets[0]
-
-
-def upsert(session, path, register_id, value):
-
-    session.transaction().execute(
-        """
-        PRAGMA TablePathPrefix("{}");
-        UPSERT INTO registers (register_id, value) VALUES
-            ( {}, "{}" );
-        """.format(path, register_id, str(value)),
-        commit_tx=True,
-    )
-
-    print("\n> append transaction:")
-    print("register, id: ", register_id, ", value: ", value)
-
-
 
 
 def select_prepared(session, path, series_id, season_id, episode_id):
@@ -178,8 +180,93 @@ def explicit_tcl(session, path, series_id, season_id, episode_id):
     tx.commit()
 
 
+def read(session, path, register_id):
+    # new transaction in serializable read write mode
+    # if query successfully completed you will get result sets.
+    # otherwise exception will be raised
+    result_sets = session.transaction(ydb.SerializableReadWrite()).execute(
+        """
+        PRAGMA TablePathPrefix("{}");
+        SELECT
+            register_id,
+            value,
+        FROM registers
+        WHERE register_id = {};
+        """.format(path, register_id),
+        commit_tx=True,
+    )
+    print("\n> read transaction:")
+    for row in result_sets[0].rows:
+        print("register, id: ", row.register_id, ", value: ", row.value)
+
+    return result_sets[0]
+
+"""
+PRAGMA TablePathPrefix("{}");
+UPDATE registers
+SET value = CAST(value ||  "1" As Utf8)
+WHERE
+    register_id = {}
+;
+"""
+
+
+def upsert(session, path, register_id, value):
+    session.transaction().execute(
+        """
+        PRAGMA TablePathPrefix("{}");
+        UPDATE registers
+        SET value = CAST(value ||  " ,{}" As Utf8)
+        WHERE
+            register_id = {}
+        ;
+        """.format(path, value, register_id),
+        commit_tx=True,
+    )
+
+    print("\n> append transaction:")
+    print("register, id: ", register_id, ", value: ", value)
+
+
+def insert_preparation(session, path, number_of_keys):
+    query = """
+            PRAGMA TablePathPrefix("{}");
+            UPSERT INTO registers
+            (
+                register_id,
+                value
+            )
+            VALUES
+            """.format(path)
+
+    for i in range(1, number_of_keys + 1):
+        if i == number_of_keys:
+            query += """
+                     (
+                        {},
+                        "0"
+                     );
+                     """.format(i)
+            continue
+        query += """
+                 (
+                    {},
+                    "0"
+                 ),
+                 """.format(i)
+
+    session.transaction().execute(
+        query,
+        commit_tx=True,
+    )
+
+    print("\n> initialize insert transaction:")
+
+
 def create_tables(session, path):
     # Creating registers table
+    session.drop_table(os.path.join(path, 'registers'))
+
     session.create_table(
         os.path.join(path, 'registers'),
         ydb.TableDescription()
@@ -189,52 +276,53 @@ def create_tables(session, path):
     )
 
 
-def describe_table(session, path, name):
-    result = session.describe_table(os.path.join(path, name))
-    print("\n> describe table: registers")
-    for column in result.columns:
-        print("column, name:", column.name, ",", str(column.type.item).strip())
+def get_read_query(path, register_id):
+    return """
+    PRAGMA TablePathPrefix("{}");
+    SELECT
+        register_id,
+        value,
+    FROM registers
+    WHERE register_id = {};
+    """.format(path, register_id)
 
 
-def bulk_upsert(table_client, path):
-    print("\n> bulk upsert: episodes")
-    column_types = ydb.BulkUpsertColumns() \
-        .add_column('series_id', ydb.OptionalType(ydb.PrimitiveType.Uint64)) \
-        .add_column('season_id', ydb.OptionalType(ydb.PrimitiveType.Uint64)) \
-        .add_column('episode_id', ydb.OptionalType(ydb.PrimitiveType.Uint64)) \
-        .add_column('title', ydb.OptionalType(ydb.PrimitiveType.Utf8)) \
-        .add_column('air_date', ydb.OptionalType(ydb.PrimitiveType.Uint64))
-    rows = basic_example_data.get_episodes_data_for_bulk_upsert()
-    table_client.bulk_upsert(os.path.join(path, "episodes"), rows, column_types)
+def get_append_query(path, register_id, value):
+    return """
+    PRAGMA TablePathPrefix("{}");
+    UPDATE registers
+    SET value = CAST(value ||  " ,{}" As Utf8)
+    WHERE
+        register_id = {}
+    ;
+    """.format(path, value, register_id)
 
 
-def is_directory_exists(driver, path):
-    try:
-        return driver.scheme_client.describe_path(path).is_directory()
-    except ydb.SchemeError:
-        return False
+def run_transaction(session, query):
+    # new transaction in serializable read write mode
+    # if query successfully completed you will get result sets.
+    # otherwise exception will be raised
+    result_sets = session.transaction(ydb.SerializableReadWrite()).execute(
+        query,
+        commit_tx=True,
+    )
+    print("\n> Results:")
+    for index, result in enumerate(result_sets):
+        print("\n> Result #{}:".format(index))
+        for row in result.rows:
+            print("register, id: ", row.register_id, ", value: ", row.value)
+
+    return result_sets
 
 
-def ensure_path_exists(driver, database, path):
-    paths_to_create = list()
-    path = path.rstrip("/")
-    while path not in ("", database):
-        full_path = os.path.join(database, path)
-        if is_directory_exists(driver, full_path):
-            break
-        paths_to_create.append(full_path)
-        path = os.path.dirname(path).rstrip("/")
-
-    while len(paths_to_create) > 0:
-        full_path = paths_to_create.pop(-1)
-        driver.scheme_client.make_directory(full_path)
-
-
-def parse_command(session, path, command):
+def get_query_by_command(path, command):
     if command[0] == 'append':
-        upsert(session, path, command[1], command[2])
+        print("\n   > append transaction:")
+        print("     register, id: ", command[1], ", value: ", command[2])
+        return get_append_query(path, command[1], command[2])
     if command[0] == 'r':
-        read(session, path, command[1])
+        print("\n   > read transaction:")
+        return get_read_query(path, command[1])
 
 
 def run(endpoint, database, path):
@@ -260,13 +348,22 @@ def run(endpoint, database, path):
 
         describe_table(session, full_path, "registers")
 
-        txns_path = "txns-small"
+        insert_preparation(session, full_path, 90)
+
+        txns_path = "txns-short-small"
 
         with open(txns_path, "r") as txns_file:
             transactions = clj.loads(txns_file.read())
+            print(transactions)
             for transaction in transactions:
-                for command in transaction['value']:
-                    parse_command(session, full_path, command)
+                print("\n> Start new transaction:")
+                query = ""
+
+                print(sorted(transaction['value'], reverse=True))
+                for command in sorted(transaction['value'], reverse=True):
+                    query += get_query_by_command(full_path, command)
+                print(query)
+                run_transaction(session, query)
 
 
         #upsert_simple(session, full_path)
